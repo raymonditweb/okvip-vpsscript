8a95c316e4f3494b33147e235a4628b7a55b4aee
Ceilo IT SEO <fdrceilo@okvip.com>
2024-11-13 15:17:22 +0700

    update to show all list blocked ips

--- script/firewall/list_blocked_ips.sh
+++ script/firewall/list_blocked_ips.sh
@@ -2,7 +2,7 @@
 
 # Kiểm tra quyền root
 if [ "$EUID" -ne 0 ]; then
-  echo "Vui lòng chạy script với quyền root."
+  echo "Error: Vui lòng chạy script với quyền root."
   exit 1
 fi
 
@@ -12,12 +12,12 @@ blocked_ips=()
 # Lấy danh sách IP bị chặn bởi UFW và thêm vào mảng
 while IFS= read -r ip; do
   blocked_ips+=("$ip")
-done < <(sudo ufw status verbose | grep -i "deny" | awk '{print $3}')
+done < <( ufw status verbose | grep -i "deny" | awk '{print $3}')
 
 # Lấy danh sách IP bị chặn bởi iptables và thêm vào mảng
 while IFS= read -r ip; do
   blocked_ips+=("$ip")
-done < <(sudo iptables -L INPUT -v -n | grep "DROP\|REJECT" | awk '{print $8}')
+done < <( iptables -L INPUT -v -n | grep "DROP\|REJECT" | awk '{print $8}')
 
 # Loại bỏ các IP trùng lặp và hiển thị danh sách cuối cùng
 echo "Danh sách IP bị chặn:"
