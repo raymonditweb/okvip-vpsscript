da2035e3afa1457a4053519adeabf5938134dd3f
Ceilo <ceilo@okvip.com>
2024-11-27 23:32:57 +0700

    update script recently packages

--- script/package/get_recently_installed_packages.sh
+++ script/package/get_recently_installed_packages.sh
@@ -6,13 +6,13 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Function to get service status and PID
+# Hàm để lấy trạng thái dịch vụ và PID
 get_service_status() {
   local service_name="$1"
   local status="Unknown"
   local pid="N/A"
 
-  # Check with systemctl
+  # Kiểm tra với systemctl
   if systemctl list-unit-files | grep -q "$service_name"; then
     local systemctl_status=$(systemctl is-active "$service_name" 2>/dev/null)
     case "$systemctl_status" in
@@ -40,40 +40,40 @@ get_service_status() {
   echo "Service Status: $status"
 }
 
-# Get the number of packages to display (default is number packages in '/var/log/dpkg.log')
+# Lấy số lượng gói cần hiển thị (mặc định là số lượng gói trong '/var/log/dpkg.log')
 num_packages="${1:-10}"
 
-# If the input is "all", fetch all packages
+# Nếu đầu vào là "all", lấy tất cả các gói
 if [ "$num_packages" == "all" ]; then
   num_packages=$(grep " install " /var/log/dpkg.log | wc -l) # Get total number of installed packages
 else
-  # Ensure the input is a number
+  # Đảm bảo đầu vào là một số
   if ! [[ "$num_packages" =~ ^[0-9]+$ ]]; then
     echo "Error: không hợp lệ. Vui lòng nhập một số hoặc 'all'."
     exit 1
   fi
 fi
 
-# Get list of recently installed packages
+# Lấy danh sách các gói được cài đặt gần đây
 recent_packages=$(grep " install " /var/log/dpkg.log | tail -n "$num_packages" | awk '{print $4}' | sort -u)
 
 echo "Information about recently installed packages:"
 
-# Get detailed information for each package
+# Lấy thông tin chi tiết cho mỗi gói
 for package in $recent_packages; do
-  # Get information from dpkg
+  # Lấy thông tin từ dpkg
   details=$(dpkg-query -s "$package" 2>/dev/null)
   if [ $? -eq 0 ]; then
-    # Extract necessary information
+    # Trích xuất thông tin cần thiết
     name=$(echo "$details" | grep '^Package:' | awk '{print $2}')
     version=$(echo "$details" | grep '^Version:' | awk '{print $2}')
     arch=$(echo "$details" | grep '^Architecture:' | awk '{print $2}')
     description=$(echo "$details" | grep '^Description:' | cut -d ':' -f2- | xargs)
 
-    # Get service status and PID
+    # Lấy trạng thái dịch vụ và PID
     service_status=$(get_service_status "$name")
 
-    # Print information
+    # Print thông tin
     echo "Package Name: $name"
     echo "Version: $version"
     echo "Architecture: $arch"
