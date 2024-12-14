64dfd60822c294f19e3bbba52ff9b2b2515ac5a9
Ceilo IT SEO <fdrceilo@okvip.com>
2024-12-14 13:30:19 +0700

    validate for alias domain

--- script/web/addon-domain.sh
+++ script/web/addon-domain.sh
@@ -7,17 +7,14 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Kiểm tra OS có phải là Ubuntu hay không
-OS=$(lsb_release -si)
-VERSION=$(lsb_release -sr)
-if [[ "$OS" != "Ubuntu" ]]; then
+if ! command -v lsb_release &>/dev/null || [[ $(lsb_release -si) != "Ubuntu" ]]; then
   echo "Error: Script này chỉ hỗ trợ Ubuntu."
   exit 1
 fi
 
 # Kiểm tra tham số đầu vào
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Error: Vui lòng cung cấp domain chính và alias domain."
-  echo "Usage: $0 <primary_domain*> <alias_domain*>"
+if [ $# -ne 2 ]; then
+  echo "Error: Vui lòng cung cấp domain chính và alias domain. Usage: $0 <primary_domain> <alias_domain>"
   exit 1
 fi
 
@@ -25,8 +22,7 @@ PRIMARY_DOMAIN=$1
 ALIAS_DOMAIN=$2
 
 # Đường dẫn cấu hình Nginx
-NGINX_CONF_DIR="/etc/nginx/sites-available"
-NGINX_CONF_FILE="$NGINX_CONF_DIR/$PRIMARY_DOMAIN"
+NGINX_CONF_FILE="/etc/nginx/sites-available/$PRIMARY_DOMAIN"
 
 # Kiểm tra website chính đã tồn tại hay chưa
 if [ ! -f "$NGINX_CONF_FILE" ]; then
@@ -34,8 +30,8 @@ if [ ! -f "$NGINX_CONF_FILE" ]; then
   exit 1
 fi
 
-# Thêm alias domain vào cấu hình Nginx
-if grep -q "$ALIAS_DOMAIN" "$NGINX_CONF_FILE"; then
+# Kiểm tra alias domain đã tồn tại hay chưa trong cấu hình Nginx
+if grep -qw "$ALIAS_DOMAIN" "$NGINX_CONF_FILE"; then
   echo "Alias domain $ALIAS_DOMAIN đã tồn tại trong cấu hình Nginx của $PRIMARY_DOMAIN."
 else
   sed -i "/server_name/s/$/ $ALIAS_DOMAIN;/" "$NGINX_CONF_FILE"
@@ -43,10 +39,8 @@ else
 fi
 
 # Kiểm tra cấu hình Nginx
-nginx -t
-if [ $? -eq 0 ]; then
+if nginx -t; then
   echo "Cấu hình Nginx hợp lệ."
-  # Tải lại Nginx để áp dụng thay đổi
   systemctl reload nginx
   echo "Nginx đã được tải lại."
 else
